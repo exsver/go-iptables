dc5799d02f62949eec94ab2fc0a8599a6a9044ac
Vadym Khomenko <exsver@gmail.com>
2024-05-03 14:37:32 +0300

    replace

--- replace.go
+++ replace.go
@@ -2,14 +2,14 @@ package iptables
 
 import "strconv"
 
-// Insert rule.
-func (c *Config) Insert(rule *Rule, num int) error {
+// Replace rule.
+func (c *Config) Replace(rule *Rule, num int) error {
 	ruleArgs, err := rule.GenArgs()
 	if err != nil {
 		return err
 	}
 
-	args := []string{"-I", c.Chain, strconv.Itoa(num)}
+	args := []string{"-R", c.Chain, strconv.Itoa(num)}
 	args = append(args, ruleArgs...)
 
 	return c.Do(args)
